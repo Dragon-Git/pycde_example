4bd962a60fc1da66c45351416042f31320b61263
Dragon-Git <townallow@outlook.com>
2023-02-13 22:11:42 +0800

    build hithub action

---
+++ fir_filter.py
@@ -0,0 +1,24 @@
+from pycde import (System, Module, Clock, Input, Output, InputChannel, OutputChannel, generator, types, dim)
+from pycde.dialects import comb, hw
+
+class Fir(Module):
+  clk = Input(types.i1)
+  rst = Input(types.i1)
+  coeff = Input(dim(32, 16))
+  data_in = Input(types.i32)
+  data_out = Output(types.i32)
+
+  @generator
+  def build(ports):
+    taps = [ports.data_in]
+    mul_list = [hw.ConstantOp(types.i32, 0)]
+    for i in range(16):
+        taps.append(taps[-1].reg(ports.clk))
+        mul_term = comb.MulOp(taps[-1],ports.coeff[i])
+        ps = comb.AddOp(mul_term, mul_list[-1])
+        mul_list.append(ps)
+    ports.data_out = ps
+
+
+mod = System([Fir],name="ip_fir_lib")
+mod.compile()
