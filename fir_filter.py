cd36be30392b6d9c43bd7a4d85fba409b226e885
Dragon-Git <1762578117@qq.com>
2023-06-20 06:18:08 +0000

    simplify fir

--- fir_filter.py
+++ fir_filter.py
@@ -1,21 +1,21 @@
-from pycde import (System, Module, Input, Output, generator, types, dim)
-from pycde.dialects import comb, hw
+from pycde import (System, Module, Input, Output, Clock, generator, dim)
+from pycde.types import Bits, SInt, UInt
 
 class Fir(Module):
-  clk = Input(types.i1)
-  rst = Input(types.i1)
+  clk = Input(Clock())
+  rst = Input(Bits(1))
   coeff = Input(dim(32, 16))
-  data_in = Input(types.i32)
-  data_out = Output(types.i32)
+  data_in = Input(SInt(32))
+  data_out = Output(SInt(32))
 
   @generator
   def build(ports):
     taps = [ports.data_in]
-    mul_list = [hw.ConstantOp(types.i32, 0)]
+    mul_list = [SInt(32)(0)]
     for i in range(16):
         taps.append(taps[-1].reg(ports.clk))
-        mul_term = comb.MulOp(taps[-1],ports.coeff[i])
-        ps = comb.AddOp(mul_term, mul_list[-1])
+        mul_term = taps[-1] * ports.coeff[i]
+        ps = mul_term + mul_list[-1]
         mul_list.append(ps)
     ports.data_out = ps
 
