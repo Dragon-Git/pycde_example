9b9332fa0bb6a6bb6eeec94bd6c96ebfb16cc0e5
Dragon-Git <townallow@outlook.com>
2023-07-09 17:54:39 +0800

    parameterized counter

--- counter.py
+++ counter.py
@@ -1,24 +1,38 @@
-from pycde import (Output, Input, Clock, Module, generator, System)
+import math
+import pycde
+from pycde import (Output, Input, Module, generator, System)
 from pycde.types import Bits, SInt, UInt  # noqa: F401
-from pycde.constructs import ControlReg, NamedWire, Reg, Wire  # noqa: F401
+from pycde.common import Clock, Reset
+from pycde.constructs import ControlReg, NamedWire, Reg, Wire, Mux  # noqa: F401
+from pycde.behavioral import If, Else, EndIf
 
-class Counter(Module):
-  clk = Clock()
-  rst = Input(Bits(1))
-  cnt = Output(UInt(32))
+@pycde.modparams
+def counter(limit, inc = 1):
+  width = math.ceil(math.log2(limit))
+  class Counter(Module):
+    clk = Clock()
+    rst = Reset()
+    cnt = Output(UInt(width))
 
-  @generator
-  def construct(ports):
-    r1 = Reg(UInt(32), ports.clk, ports.rst)
-    r1.name = "counter"
-    ports.cnt = r1
-    r1.assign((r1 + 1).as_uint(32))
-    #  An alternative implementation
-    # w1 = Wire(UInt(32))
-    # r1 = w1.reg(ports.clk, ports.rst)
-    # w1.assign((r1 + 1).as_uint(32)) # increment the internal counter
-    # ports.cnt = r1
+    @generator
+    def construct(ports):
+      counter = Reg(UInt(width), rst = ports.rst)
+      counter.name = "count"
+      ports.cnt = counter
+      # counter.assign(Mux((counter == UInt(width)(limit)).as_bits(1), (counter + inc).as_uint(width), UInt(width)(0)))
+      cnt = (counter + inc).as_uint(width)
+      with If((counter == UInt(width)(limit)).as_bits(1)):
+        cnt = UInt(width)(0)
+      EndIf()
+      counter.assign(cnt)
+      #  An alternative implementation
+      # w1 = Wire(UInt(32))
+      # r1 = w1.reg(ports.clk, ports.rst)
+      # w1.assign((r1 + 1).as_uint(32)) # increment the internal counter
+      # ports.cnt = r1
+  return Counter
+
 
 if __name__ == '__main__':
-  mod = System([Counter],name="ip_cnt_lib", output_directory="build/ip_cnt_lib")
+  mod = System([counter(58642)],name="ip_cnt_lib", output_directory="build/ip_cnt_lib")
   mod.compile()
