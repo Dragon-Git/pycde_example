decf6cb425dd6b811740ba323dcc2f921b48e83a
Dragon-Git <1762578117@qq.com>
2023-07-26 06:05:46 +0000

    update lfsr

--- lfsr.py
+++ lfsr.py
@@ -4,7 +4,7 @@ from pycde.constructs import ControlReg, NamedWire, Reg, Wire  # noqa: F401
 from pycde.dialects import comb
 
 POLY = [7, 5, 4, 3]
-WIDTH = max(POLY) +1
+WIDTH = max(POLY) + 1
 
 class Lfsr(Module):
   clk = Clock()
@@ -17,7 +17,7 @@ class Lfsr(Module):
     r1 = Reg(Bits(WIDTH), io.clk, io.rst, io.seed)
     r1.name = "Lfsr"
     w2 = comb.XorOp(*[r1[i] for i in POLY])
-    w1.assign(comb.ConcatOp(r1[0:-1], w2))
+    r1.assign(comb.ConcatOp(r1[0:-1], w2))
     io.cnt = r1.as_uint()
 
 if __name__ == '__main__':
